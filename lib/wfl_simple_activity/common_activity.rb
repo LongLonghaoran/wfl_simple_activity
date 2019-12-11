45aeb846dc034b8a4250a7b34fd06793c0075edb
longhaoran <longhr@wfl-ischool.cn>
2019-12-11 11:40:58 +0800

    add rollback hooks

--- lib/wfl_simple_activity/common_activity.rb
+++ lib/wfl_simple_activity/common_activity.rb
@@ -11,5 +11,13 @@ module CommonActivity
         request_params: proc {|controller, model| controller&.params&.to_h}
       }
     )
+
+    has_many :activities, as: :trackable, class_name: 'PublicActivity::Activity', dependent: :destroy
+
+    after_rollback proc {
+      self.create_activity key: "#{self.class.name.underscore}.failed", recipient: self
+      # 由于这里无法获取controller,需要自行采用其他方式来获取current_user,比如: 在oa-saas中有个Thread.current保存了全局的变量
+      # self.create_activity key: "#{self.class.name.underscore}.failed", owner: User.find(Thread.current[:user_id]), recipient: self
+    }
   end
 end
