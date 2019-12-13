97e7979c86f2a5737a1e93a30fc7b944d1e72a5c
longhaoran <longhr@wfl-ischool.cn>
2019-12-13 15:24:44 +0800

    0.1.10

--- lib/generators/wfl_simple_activity/templates/common_activity.rb
+++ lib/generators/wfl_simple_activity/templates/common_activity.rb
@@ -10,5 +10,11 @@ module CommonActivity
         request_params: proc {|controller, model| controller&.params&.to_h}
       }
     )
+
+    has_many :activities, as: :trackable, class_name: 'PublicActivity::Activity', dependent: :destroy
+
+    after_rollback proc {
+      self.create_activity key: "#{self.class.name.underscore}.failed", owner: User.find(Thread.current[:user_id]), recipient: self
+    }
   end
 end
