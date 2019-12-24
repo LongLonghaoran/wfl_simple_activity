db2bcfa712d035e6ffff2fd6c9d51dd2870cb716
longhaoran <longhr@wfl-ischool.cn>
2019-12-24 18:36:56 +0800

    fix table_name

--- lib/generators/wfl_simple_activity/templates/activity.rb
+++ lib/generators/wfl_simple_activity/templates/activity.rb
@@ -1,6 +1,7 @@
+require "public_activity/orm/active_record/activity.rb"
+PublicActivity::ORM::ActiveRecord::Activity.table_name = "wfl_activities"
 module PublicActivity
   class Activity
-    self.table_name = "wfl_activities"
     serialize :parameters, Hash
     serialize :model_changes, Hash
   end
