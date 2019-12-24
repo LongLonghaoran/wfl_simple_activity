6f69ae45f199d16ae6ccd05fa78b0bfe39d8e697
longhaoran <longhr@wfl-ischool.cn>
2019-12-24 16:56:26 +0800

    表名更改

--- lib/generators/wfl_simple_activity/templates/activity.rb
+++ lib/generators/wfl_simple_activity/templates/activity.rb
@@ -1,5 +1,6 @@
 module PublicActivity
   class Activity
+    self.table_name = "wfl_activities"
     serialize :parameters, Hash
     serialize :model_changes, Hash
   end
--- lib/generators/wfl_simple_activity/templates/create_activities.rb
+++ lib/generators/wfl_simple_activity/templates/create_activities.rb
@@ -2,22 +2,24 @@
 class CreateActivities < ActiveRecord::Migration
   # Create table
   def self.up
-    create_table :activities do |t|
+    create_table :wfl_activities do |t|
       t.belongs_to :trackable, :polymorphic => true
       t.belongs_to :owner, :polymorphic => true
       t.string  :key
       t.text    :parameters
       t.belongs_to :recipient, :polymorphic => true
       t.text :model_changes
+      t.integer :school_id
+      t.string :description
       t.timestamps
     end
 
-    add_index :activities, [:trackable_id, :trackable_type]
-    add_index :activities, [:owner_id, :owner_type]
-    add_index :activities, [:recipient_id, :recipient_type]
+    add_index :wfl_activities, [:trackable_id, :trackable_type]
+    add_index :wfl_activities, [:owner_id, :owner_type]
+    add_index :wfl_activities, [:recipient_id, :recipient_type]
   end
   # Drop table
   def self.down
-    drop_table :activities
+    drop_table :wfl_activities
   end
 end
